cb06afd871237f409602a98f2b37ad87de74aabc
David <dtadevosyan53@gmail.com>
2024-02-24 13:07:13 +0400

    added some commnets

--- Admin/upload_to_inst.py
+++ Admin/upload_to_inst.py
@@ -2,17 +2,24 @@ import requests
 import json
 import time
 
+# Graph API base URL
 graph_url = 'https://graph.facebook.com/v19.0/'
 
+# Function to post a reel on Instagram
 def post_reel(caption='Test Caption', media_type='REELS', share_to_feed='', thumb_offset='',
               video_url='https://davidtadevosyan.publit.io/file/second-largest-task.mp4'):
+    # Load configuration from JSON file
     with open('graph_api_param.json', 'r') as f:
         config = json.load(f)
     
+    # Extract access token and Instagram account ID from config
     access_token = config['access_token']
     instagram_account_id = config['instagram_account_id']
     
+    # Construct URL for posting media
     url = graph_url + instagram_account_id + '/media'
+    
+    # Parameters for the request
     param = {
         'access_token': access_token,
         'caption': caption,
@@ -21,15 +28,19 @@ def post_reel(caption='Test Caption', media_type='REELS', share_to_feed='', thum
         'thumb_offset': thumb_offset,
         'video_url': video_url
     }
+    
     try:
+        # Send POST request to post the reel
         response = requests.post(url, params=param)
         response_json = response.json()
         print("\nResponse from post_reel:", response_json)
         return response_json
     except requests.RequestException as e:
+        # Handle request exceptions
         print("Error posting reel:", e)
         return None
 
+# Function to check the status of upload
 def status_of_upload(ig_container_id=''):
     with open('graph_api_param.json', 'r') as f:
         config = json.load(f)
@@ -50,6 +61,7 @@ def status_of_upload(ig_container_id=''):
         print("Error getting upload status:", e)
         return None
 
+# Function to publish the container
 def publish_container(creation_id=''):
     with open('graph_api_param.json', 'r') as f:
         config = json.load(f)
@@ -71,17 +83,21 @@ def publish_container(creation_id=''):
         print("Error publishing container:", e)
         return None
 
-# Run functions
+# Main function
 if __name__ == "__main__":
+    # Post a reel
     response_post_reel = post_reel()
     if response_post_reel:
+        # Extract Instagram container ID from the response
         ig_container_id = response_post_reel.get('id')
         if ig_container_id:
             max_retries = 10  # Adjust as needed
             retries = 0
             while retries < max_retries:
+                # Check the status of upload
                 response_status_of_upload = status_of_upload(ig_container_id)
                 if response_status_of_upload and response_status_of_upload.get('status_code') == 'FINISHED':
+                    # Publish the container if upload is finished
                     response_publish_container = publish_container(ig_container_id)
                     if response_publish_container:
                         print("Reel successfully published!")
