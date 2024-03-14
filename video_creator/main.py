8c273e908042bad613569fe2a2652ef4e2b045d1
David <dtadevosyan53@gmail.com>
2024-03-14 19:28:16 +0400

    separated downloading and api interaciton methods

--- video_creator/main.py
+++ video_creator/main.py
@@ -1,14 +1,21 @@
-#main.py
 from video_downloader import PexelsVideoDownloader
-from concatenate_videos import VideoConcatenator 
+from video_cutter import VideoCutter
+from merge import VideoMerger
 
-def main():
-    downloader = PexelsVideoDownloader()
+def main(): 
+    # Calling video downloader
+    downloader = PexelsVideoDownloader()  # Create an instance of PexelsVideoDownloader
     downloader.main()
-    folder_path = "footages"  # Adjust the folder path accordingly
-    concatenator = VideoConcatenator(folder_path)
-    concatenator.concatenate_videos()
+    
+    # Cutting the videos to 5-second long videos
+    cutter = VideoCutter()
+    downloaded_video_paths, total_duration = cutter.cut_videos()
+    print("Videos downloaded and cut successfully:", downloaded_video_paths, total_duration)
+
+    # Merging cut videos
+    merger = VideoMerger(video_dir="cut_videos", output_path="merged_video.mp4")
+    merger.merge_videos()
+    print("Videos downloaded, cut, and merged successfully.")
 
 if __name__ == "__main__":
     main()
-
--- video_creator/video_downloader_test.py
+++ video_creator/video_downloader_test.py
@@ -3,7 +3,7 @@ import random
 import requests
 import json
 from tqdm import tqdm
-
+from urllib.parse import urlparse, parse_qs
 class VideoDownloader:
     """
     A class for downloading videos from Pexels.com.
@@ -24,7 +24,7 @@ class VideoDownloader:
             str: The Pexels API key.
         """
         try:
-            with open('pexels_api.json') as file:
+            with open('pexels_api_2.json') as file:
                 config = json.load(file)
                 return config["pexels_api_key"]
         except FileNotFoundError:
@@ -34,23 +34,22 @@ class VideoDownloader:
             print("Error: The 'pexels_api_key' was not found in the pexels_api.json file.")
             return None
 
-    def search_videos(self, theme, per_page=80, total_videos=1000):
+    def search_videos(self, theme, total_videos=1000):
         """
         Searches for videos on Pexels based on the specified theme.
 
         Args:
             theme (str): The theme to search for.
-            per_page (int): Number of videos per page (default is 80).
             total_videos (int): Total number of videos to retrieve (default is 1000).
 
         Returns:
             list: A list of video data returned by the API.
         """
         try:
-            all_videos = []
+            videos = []
             page = 1
-            while len(all_videos) < total_videos:
-                url = f'https://api.pexels.com/videos/search?query={theme}&per_page={per_page}&page={page}'
+            while len(videos) < total_videos:
+                url = f'https://api.pexels.com/videos/search?query={theme}&per_page=80&page={page}'
                 headers = {'Authorization': self.api_key}
                 response = requests.get(url, headers=headers)
                 response.raise_for_status()
@@ -58,26 +57,20 @@ class VideoDownloader:
                 page_videos = data.get('videos', [])
                 if not page_videos:
                     break
-                all_videos.extend(page_videos)
+                videos.extend(page_videos)
                 page += 1
-
-            print("Status Code:", response.status_code)
-            print("Successful API Response:")
-            print("Number of videos Found:", len(all_videos))
-            print("Videos")
-            for video in all_videos[:total_videos]:
-                print("Video ID:", video.get('id'))
-                print("Video URL:", video.get('url'))
-                print("Video Duration:", video.get('duration'))
-                print("Video Quality:", video.get('video_files')[0].get('quality'))
-                print("Video Width:", video.get('video_files')[0].get('width'))
-                print("Video Height:", video.get('video_files')[0].get('height'))
-                print("-" * 50)
-
-            return all_videos
+                next_page = data.get('next_page')
+                if not next_page:
+                    break
+                page_url = urlparse(next_page)
+                page_query = parse_qs(page_url.query) 
+                page = int(page_query['page'][0])
+            print("Total videos found:", len(videos))
+            return videos[:total_videos]
         except requests.RequestException as e:
-            print(f"Error Searching for videos: {e}")
+            print(f"Error searching for videos: {e}")
             return []
+
 
     def download_videos(self, theme, num_videos=10):
         """
