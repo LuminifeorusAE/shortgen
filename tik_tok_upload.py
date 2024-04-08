5e3069ee3e9f6caa7c2525f89a39438ae6201963
David <dtadevosyan53@gmail.com>
2024-04-09 02:19:38 +0400

    empty commit

--- tik_tok_upload.py
+++ tik_tok_upload.py
@@ -1,29 +1,33 @@
 import os
 from tiktok_uploader.upload import upload_video
 
-class TikTokUploader:
-    def __init__(self, video_directory, cookies_file="cookies.txt"):
-        self.video_directory = video_directory
-        self.cookies_file = cookies_file
+VIDEO_DIRECTORY = "final_videos"
 
-    def upload_first_video(self):
+def upload_video_from_directory():
+    try:
         # Get a list of files in the directory
-        files = os.listdir(self.video_directory)
+        files = os.listdir(VIDEO_DIRECTORY)
+        print("Files in the directory:", files)
 
         # Filter out only the .mp4 files
         mp4_files = [file for file in files if file.endswith(".mp4")]
+        print("MP4 files found:", mp4_files)
 
         if mp4_files:
             # Take the first .mp4 file found
-            video_path = os.path.join(self.video_directory, mp4_files[0])
+            video_path = os.path.join(VIDEO_DIRECTORY, mp4_files[0])
+            print("Selected video:", video_path)
 
             # Upload the video to TikTok
+            print("Uploading video...")
             upload_video(video_path,
                          description="This is a video from the local directory",
-                         cookies=self.cookies_file)
+                         cookies="cookies.txt", headless=True)
+            print("Upload completed.")
         else:
             print("No .mp4 files found in the directory.")
+    except Exception as e:
+        print("An error occurred during video upload:", str(e))
 
 if __name__ == "__main__":
-    uploader = TikTokUploader(video_directory="final_videos")
-    uploader.upload_first_video()
+    upload_video_from_directory()
